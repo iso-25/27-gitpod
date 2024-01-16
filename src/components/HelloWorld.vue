1b80d24b695f06ff59a4d23c52456fb6dbb702b9
iso-25 <i.s.o@i.ua>
2024-01-16 18:41:54 +0200

    commit

--- src/components/HelloWorld.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <h1>App component</h1>
-</template>
-
-<script>
-export default {
-  name: 'App',
-}
-</script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/HomePage.vue';
-import About from '@/views/AboutPage.vue';
+// import About from '@/views/AboutPage.vue';
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/about', component: About },
+  // { path: '/about', component: About },
 ];
 
 const router = createRouter({
